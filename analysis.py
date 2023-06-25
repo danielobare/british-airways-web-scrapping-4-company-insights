42a788a42a672c1edcfda128d2b9385a7eb37918
Daniel Obare <dnobare@gmail.com>
2023-06-25 23:35:18 +0300

    included code to add title and export to hml file

--- analysis.py
+++ analysis.py
@@ -4,3 +4,8 @@ from ydata_profiling import ProfileReport
 df = pd.read_csv('BA_reviews_1.csv')
 
 print (df.head())
+
+profile = ProfileReport(df, title - 'BA Review Analysis')
+
+profile.to_file('BA_Review_Analysis.html')
+
