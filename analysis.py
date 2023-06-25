3d35b85bcb6e07235133cc20ba57e50794e9b2e0
Daniel Obare <dnobare@gmail.com>
2023-06-25 23:37:04 +0300

    replaced - with =

--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ df = pd.read_csv('BA_reviews_1.csv')
 
 print (df.head())
 
-profile = ProfileReport(df, title - 'BA Review Analysis')
+profile = ProfileReport(df, title = 'BA Review Analysis')
 
 profile.to_file('BA_Review_Analysis.html')
 
